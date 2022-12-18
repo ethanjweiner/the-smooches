2a0b91b39afc17c3d1e09270f67e6f00d1f5b155
Ethan Weiner <ethanweiner20@gmail.com>
2022-12-18 12:00:51 -0500

    test: basic test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Slideshows buttons', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Slideshows'));
+  expect(screen.getByText('Upload'));
 });
