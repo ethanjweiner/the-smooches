67fbb4b493eefd9c8eb740ef2283fdd0726a8985
Ethan Weiner <ethanweiner20@gmail.com>
2022-12-23 13:02:42 -0500

    chore: always restart frontend container

--- scripts/configure-ec2.sh
+++ scripts/configure-ec2.sh
@@ -6,5 +6,6 @@ cp ./docker-compose.yml ./smooches
 cp ./docker-compose.production.yml ./smooches
 cp ./scripts/production.sh ./smooches
 cp ./backend/.env.prod ./smooches/backend
+ssh -i "./aws/ec2-key-pair.cer" ubuntu@ec2-54-89-237-229.compute-1.amazonaws.com rm -rf ./smooches
 scp -r -i "./aws/ec2-key-pair.cer" ./smooches ubuntu@ec2-54-89-237-229.compute-1.amazonaws.com:~
 rm -r ./smooches
