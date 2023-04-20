b761def7d2f0081e6f56a2d19eb2d10c08d0ac5f
Ethan Weiner <ethanweiner20@gmail.com>
2023-04-19 22:25:02 -0400

    fix: update ip

--- scripts/deployment/configure.sh
+++ scripts/deployment/configure.sh
@@ -3,7 +3,7 @@
 # Configure the EC2 instance to be ready for production
 
 # Stop docker
-ssh -i "./aws/ec2-key-pair.cer" ubuntu@18.210.251.134 \
+ssh -i "./aws/ec2-key-pair.cer" ubuntu@te-1.amazonaws.com	54.211.204.107 \
   "cd ~/smooches && sudo docker compose down"
 
 # Copy files needed for production
@@ -14,11 +14,11 @@ cp -r ./scripts/production/* ./smooches
 cp ./backend/.env.prod ./smooches/backend
 
 # Replace the project directory on the server
-ssh -i "./aws/ec2-key-pair.cer" ubuntu@18.210.251.134 rm -rf ./smooches
-scp -r -i "./aws/ec2-key-pair.cer" ./smooches ubuntu@18.210.251.134:~
+ssh -i "./aws/ec2-key-pair.cer" ubuntu@te-1.amazonaws.com	54.211.204.107 rm -rf ./smooches
+scp -r -i "./aws/ec2-key-pair.cer" ./smooches ubuntu@te-1.amazonaws.com	54.211.204.107:~
 
 # Remove the temporary local directory
 rm -r ./smooches
 
 # Pull the images onto the server and run the production scripts
-ssh -i "./aws/ec2-key-pair.cer" ubuntu@18.210.251.134 "cd ~/smooches && sudo sh ./pull.sh && sudo sh ./start.sh"
+ssh -i "./aws/ec2-key-pair.cer" ubuntu@te-1.amazonaws.com	54.211.204.107 "cd ~/smooches && sudo sh ./pull.sh && sudo sh ./start.sh"
